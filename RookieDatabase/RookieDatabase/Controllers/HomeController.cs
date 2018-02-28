57f390082f052b740ea4927e9c1d80b754103fc0
Ty <TSBukowski@yahoo.com>
2018-02-27 21:57:07 -0600

    updated rookie database

--- RookieDatabase/RookieDatabase/Controllers/HomeController.cs
+++ RookieDatabase/RookieDatabase/Controllers/HomeController.cs
@@ -36,54 +36,58 @@ namespace RookieDatabase.Controllers
         //PLAYER NAMES LINK TO PLAYER EDIT VIEW, PLAYER SEARCH
 
         //[Route("Home/Position")] --Do not need if action is named correctly
-        public IActionResult Position(RookieViewModel player)
+        public IActionResult Position(string position)
         {
-            var vm = new RookieViewModel
+            var vm = new PositionViewModel();
+            var rookieList = _context.Player.ToList();
+            var rookies = rookieList.Select(r => new PositionDTO
             {
-                PlayerName = player.PlayerName,
-                Position = player.Position,
-                Age = player.Age,
-                Height = player.Height,
-                Weight = player.Weight,
-                Development = player.Development,
-                OVR = player.OVR,
-                SPD = player.SPD,
-                ACC = player.ACC,
-                STR = player.STR,
-                AGI = player.AGI,
-                ELU = player.ELU,
-                BCV = player.BCV,
-                CAR = player.CAR,
-                JKM = player.JKM,
-                SPM = player.SPM,
-                SFA = player.SFA,
-                TRK = player.TRK,
-                CTH = player.CTH,
-                CIT = player.CIT,
-                SPC = player.SPC,
-                RTE = player.RTE,
-                RLS = player.RLS,
-                JMP = player.JMP,
-                THP = player.THP,
-                SAC = player.SAC,
-                MAC = player.MAC,
-                DAC = player.DAC,
-                RUN = player.RUN,
-                PAC = player.PAC,
-                RBK = player.RBK,
-                PBK = player.PBK,
-                IBL = player.IBL,
-                TAK = player.TAK,
-                POW = player.POW,
-                BSH = player.BSH,
-                FMV = player.FMV,
-                PMV = player.PMV,
-                MCV = player.MCV,
-                ZCV = player.ZCV,
-                PRS = player.PRS,
-                PRC = player.PRC,
-                PUR = player.PUR
-            };
+                PlayerName = r.PlayerName,
+                Position = r.Position,
+                Age = r.Age,
+                Height = r.Height,
+                Weight = r.Weight,
+                Development = r.Development,
+                OVR = r.OVR,
+                SPD = r.SPD,
+                ACC = r.ACC,
+                STR = r.STR,
+                AGI = r.AGI,
+                ELU = r.ELU,
+                BCV = r.BCV,
+                CAR = r.CAR,
+                JKM = r.JKM,
+                SPM = r.SPM,
+                SFA = r.SFA,
+                TRK = r.TRK,
+                CTH = r.CTH,
+                CIT = r.CIT,
+                SPC = r.SPC,
+                RTE = r.RTE,
+                RLS = r.RLS,
+                JMP = r.JMP,
+                THP = r.THP,
+                SAC = r.SAC,
+                MAC = r.MAC,
+                DAC = r.DAC,
+                RUN = r.RUN,
+                PAC = r.PAC,
+                RBK = r.RBK,
+                PBK = r.PBK,
+                IBL = r.IBL,
+                TAK = r.TAK,
+                POW = r.POW,
+                BSH = r.BSH,
+                FMV = r.FMV,
+                PMV = r.PMV,
+                MCV = r.MCV,
+                ZCV = r.ZCV,
+                PRS = r.PRS,
+                PRC = r.PRC,
+                PUR = r.PUR
+            });
+            vm.Rookies = rookies;
+
             return View(vm);
         }
 
---
+++ RookieDatabase/RookieDatabase/ViewModels/PositionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieDatabase.ViewModels
+{
+    public class PositionViewModel
+    {
+        public IEnumerable<PositionDTO> Rookies { get; set; }
+    }
+}
